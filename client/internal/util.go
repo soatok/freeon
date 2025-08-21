c9ef889225cf57538493ddc0d12e5fd3aacb284c
Soatok <soatok.dhole@gmail.com>
2025-08-21 18:09:53 -0400

    add tests for finalization election

--- client/internal/util.go
+++ client/internal/util.go
@@ -46,6 +46,19 @@ func UniqueID() (string, error) {
 	return hex.EncodeToString(b), nil
 }
 
+// Determine index from hash + party size.
+//
+// We sample 64 bits and reduce by x bits (where x <= 16).
+// This gives us n+48 bits reduced modulo an n-bit number, which minimizes modulo bias.
+func SelectIndex(ch []byte, partySize uint64) uint64 {
+	if len(ch) < 8 {
+		panic("ch must be at least 8 bytes long")
+	}
+	buf := ch[len(ch)-8:]
+	unbiased := binary.BigEndian.Uint64(buf)
+	return unbiased % partySize
+}
+
 // Given a hash, party ID, and list of all party IDs:
 // Select a pseudorandom element of party.
 // The last 8 bytes of hash are converted to a uint64, then reduced modulo the size of the party
@@ -54,10 +67,12 @@ func AmIElected(ch []byte, me uint16, party []uint16) bool {
 	if len(party) < 2 {
 		return true
 	}
-	buf := ch[len(ch)-8:]
-	unbiased := binary.BigEndian.Uint64(buf)
+	if !slices.Contains(party, me) {
+		// If you are not a member, you are not chosen
+		return false
+	}
 	ps := uint64(len(party))
-	index := unbiased % ps
+	index := SelectIndex(ch, ps)
 	return party[index] == me
 }
 
--- client/internal/util_test.go
+++ client/internal/util_test.go
@@ -1,6 +1,8 @@
 package internal_test
 
 import (
+	"crypto/sha512"
+	"fmt"
 	"testing"
 
 	"github.com/soatok/freon/client/internal"
@@ -24,4 +26,65 @@ func TestHashMessageForSanity(t *testing.T) {
 	assert.NotEqual(t, hash2, hash3)
 	assert.NotEqual(t, hash2, hash4)
 	assert.NotEqual(t, hash3, hash4)
+}
+
+func TestAmIElected(t *testing.T) {
+	hash := sha512.Sum384([]byte("freon testing"))
+	tests := []struct {
+		Elected bool
+		MyID    uint16
+		Party   []uint16
+	}{
+		{true, 1, []uint16{1}},
+
+		{true, 1, []uint16{1, 2}},
+		{false, 2, []uint16{1, 2}},
+		{false, 0xffff, []uint16{1, 2}},
+
+		{false, 1, []uint16{1, 2, 3}},
+		{true, 2, []uint16{1, 2, 3}},
+		{false, 3, []uint16{1, 2, 3}},
+
+		{true, 1, []uint16{1, 2, 3, 4}},
+		{false, 2, []uint16{1, 2, 3, 4}},
+		{false, 3, []uint16{1, 2, 3, 4}},
+		{false, 4, []uint16{1, 2, 3, 4}},
+
+		{false, 1, []uint16{1, 2, 3, 4, 5}},
+		{false, 2, []uint16{1, 2, 3, 4, 5}},
+		{true, 3, []uint16{1, 2, 3, 4, 5}},
+		{false, 4, []uint16{1, 2, 3, 4, 5}},
+		{false, 5, []uint16{1, 2, 3, 4, 5}},
+
+		{false, 1, []uint16{1, 2, 3, 4, 5, 6}},
+		{false, 2, []uint16{1, 2, 3, 4, 5, 6}},
+		{false, 3, []uint16{1, 2, 3, 4, 5, 6}},
+		{false, 4, []uint16{1, 2, 3, 4, 5, 6}},
+		{true, 5, []uint16{1, 2, 3, 4, 5, 6}},
+		{false, 6, []uint16{1, 2, 3, 4, 5, 6}},
+
+		{false, 1, []uint16{1, 2, 3, 4, 5, 6, 7}},
+		{false, 2, []uint16{1, 2, 3, 4, 5, 6, 7}},
+		{false, 3, []uint16{1, 2, 3, 4, 5, 6, 7}},
+		{false, 4, []uint16{1, 2, 3, 4, 5, 6, 7}},
+		{false, 5, []uint16{1, 2, 3, 4, 5, 6, 7}},
+		{true, 6, []uint16{1, 2, 3, 4, 5, 6, 7}},
+		{false, 7, []uint16{1, 2, 3, 4, 5, 6, 7}},
+
+		{false, 1, []uint16{1, 2, 3, 4, 5, 6, 7, 8}},
+		{false, 2, []uint16{1, 2, 3, 4, 5, 6, 7, 8}},
+		{false, 3, []uint16{1, 2, 3, 4, 5, 6, 7, 8}},
+		{false, 4, []uint16{1, 2, 3, 4, 5, 6, 7, 8}},
+		{true, 5, []uint16{1, 2, 3, 4, 5, 6, 7, 8}},
+		{false, 6, []uint16{1, 2, 3, 4, 5, 6, 7, 8}},
+		{false, 7, []uint16{1, 2, 3, 4, 5, 6, 7, 8}},
+		{false, 8, []uint16{1, 2, 3, 4, 5, 6, 7, 8}},
+	}
+	for _, tt := range tests {
+		res := internal.AmIElected(hash[:], tt.MyID, tt.Party)
+		if tt.Elected != res {
+			fmt.Printf("id = %d, ids = %d\n", tt.MyID, tt.Party)
+		}
+		assert.Equal(t, res, tt.Elected)
+	}
 }
